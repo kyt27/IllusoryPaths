2f8287a4bc4420879337a95bcfba21093d398662
Sriram <asriram830@gmail.com>
2024-03-10 19:23:16 +0000

    New Level

--- Assets/Scripts/NodeInteractables/GlobalRotate.cs
+++ Assets/Scripts/NodeInteractables/GlobalRotate.cs
@@ -15,25 +15,54 @@ public class GlobalRotate : BaseNodeInteractable
 
     public List<RotationObj> rotateTogether = new List<RotationObj>();
     private RotationLinker rotationLinker;
+    public float rotationSpeed = 1;
+    private float progress = 0;
+    private bool rotating = false;
+    public NavController player;
+    public PlayerAnimation playerAnimation;
+
     // Start is called before the first frame update
     void Start()
     {
-        rotationLinker = GetComponent<RotationLinker>();   
+        rotationLinker = GetComponent<RotationLinker>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKeyDown("space")) {
             Rotate();
         }
+        if (rotating) {
+            doRotate();
+            if (progress >= 1) {
+                lockAngles();
+                rotating = false;
+                rotationLinker.UpdateRotationLinks();
+                progress = 0;
+            }
+        }
     }
 
     void Rotate() {
+        rotating = true;
+    }
+
+    void lockAngles() {
         foreach(RotationObj obj in rotateTogether) {
-            obj.obj.transform.RotateAround(obj.rotatePoint.position, obj.axis, obj.angle);
+            obj.obj.transform.RotateAround(obj.rotatePoint.position, obj.axis, -1 * obj.angle * (progress - 1));
         }
-        rotationLinker.UpdateRotationLinks();
+    }
+
+    void doRotate() {
+        player.SnapToCurrentNode();
+        playerAnimation.ToggleAnimation(false);
+        float inc = Time.deltaTime * rotationSpeed;
+        foreach(RotationObj obj in rotateTogether) {
+            obj.obj.transform.RotateAround(obj.rotatePoint.position, obj.axis, obj.angle * inc);
+        }
+        progress += inc;
+        
     }
 
     public override void Action(Vector3 position) {
