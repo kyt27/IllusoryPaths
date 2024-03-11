7aa5a5e0a797c86ce07e7bd837075cdf14ed5bde
Sriram <asriram830@gmail.com>
2024-03-11 21:28:27 +0000

    Added Textures

--- Assets/Scripts/NodeInteractables/KeyVanish.cs
+++ Assets/Scripts/NodeInteractables/KeyVanish.cs
@@ -6,8 +6,14 @@ public class KeyVanish : BaseNodeInteractable {
     public Node node;
 
     bool activated = false;
+    public ParticleSystem particles;
+
+    void Awake() {
+        particles.Stop();
+    }
 
     public override void Action(Vector3 myInput) {
+        particles.Play();
         if(!activated && node != null) {
             node.EnableNode();
             Destroy(gameObject);
